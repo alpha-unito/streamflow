3de01f2eb324fbb16f80e1f135e5a1b970f11be0
GlassOfWhiskey <iacopo.c92@gmail.com>
2022-04-15 17:52:35 +0200

    Updated ANTLR version to 4.10
    ANTLR runtime version 4.10 is not compatible with scripts generated with
    previous version. Given that, all ANTLR scripts have been regenerated
    from the ECMAScript grammar file. Plus, a script has been added to make
    this process more clear and easily available for future usage.
    Finally, given the incompatibility between ANTLR versions, a requirement
    for an ANTLR runtime of version >=4.10 has been added to the `setup.py`.

---
+++ scripts/antlr4-generator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ANTLR4_VERSION="${1:-4.10}"
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WORKDIR=$(mktemp -d)
+
+cd "${WORKDIR}" || exit
+curl -fsSLO "https://www.antlr.org/download/antlr-${ANTLR4_VERSION}-complete.jar"
+curl -fsSLO "https://raw.githubusercontent.com/antlr/grammars-v4/master/javascript/ecmascript/Python/ECMAScript.g4"
+java -jar "antlr-${ANTLR4_VERSION}-complete.jar" -Dlanguage=Python3 ECMAScript.g4
+mv ./*.py "${SCRIPT_DIRECTORY}/../streamflow/cwl/antlr/"
+rm -rf "${WORKDIR}"
