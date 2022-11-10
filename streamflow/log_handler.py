c1921f3ae09848fce7d9af65a2655d4bacdc6762
Paolo Viviani <paolo.viviani@linksfoundation.com>
2022-11-10 09:36:04 +0000

    parametrized log format

--- streamflow/log_handler.py
+++ streamflow/log_handler.py
@@ -1,9 +1,12 @@
 import logging
 
+logFormat = '%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s'
+dateFormat = '%Y-%m-%d %H:%M:%S'
+
 logger = logging.getLogger("streamflow")
 defaultStreamHandler = logging.StreamHandler()
-formatter = logging.Formatter(fmt='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
-                              datefmt='%Y-%m-%d %H:%M:%S')
+formatter = logging.Formatter(fmt=logFormat,
+                              datefmt=dateFormat)
 defaultStreamHandler.setFormatter(formatter)
 logger.addHandler(defaultStreamHandler)
 logger.setLevel(logging.INFO)
