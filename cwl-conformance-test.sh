129bcb2f0152bb196cc66229b26bff863f077803
GlassOfWhiskey <iacopo.c92@gmail.com>
2023-01-08 20:50:59 +0100

    Replaced file extension

--- cwl-conformance-test.sh
+++ cwl-conformance-test.sh
@@ -56,6 +56,7 @@ if [[ "${VERSION}" = "v1.0" ]] ; then
 else
   CONFORMANCE_TEST="${SCRIPT_DIRECTORY}/${REPO}-${COMMIT}/conformance_tests.yaml"
 fi
+CONFORMANCE_TEST="${CONFORMANCE_TEST%".yaml"}.cwltest.yaml"
 
 # Build command
 TEST_COMMAND="python -m pytest ${CONFORMANCE_TEST} -n auto -rs"
