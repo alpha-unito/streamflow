7ba71be532ce91c76701a250358f395e43c62dcd
Alberto Mulone <alberto.mulone@edu.unito.it>
2023-10-09 18:02:50 +0200

    added possibility to choose checkpoint data location (local or remote)

--- streamflow/recovery/checkpoint_manager.py
+++ streamflow/recovery/checkpoint_manager.py
@@ -11,6 +11,7 @@ import pkg_resources
 from streamflow.core import utils
 from streamflow.core.data import DataLocation
 from streamflow.core.deployment import LOCAL_LOCATION, Location
+from streamflow.core.exception import WorkflowDefinitionException
 from streamflow.core.recovery import CheckpointManager
 from streamflow.core.utils import random_name
 from streamflow.core.workflow import Token
@@ -23,7 +24,13 @@ if TYPE_CHECKING:
 
 
 class DefaultCheckpointManager(CheckpointManager):
-    def __init__(self, context: StreamFlowContext, checkpoint_dir: str | None = None):
+    def __init__(
+        self,
+        context: StreamFlowContext,
+        checkpoint_dir: str | None = None,
+        deployment: str = None,
+        service: str = None,
+    ):
         super().__init__(context)
         self.checkpoint_dir = checkpoint_dir or os.path.join(
             os.path.realpath(tempfile.gettempdir()),
@@ -31,14 +38,27 @@ class DefaultCheckpointManager(CheckpointManager):
             "checkpoint",
             utils.random_name(),
         )
+        if deployment is None:
+            self.deployment_name = LOCAL_LOCATION
+        elif deployment in context.config.get("deployments"):
+            self.deployment_name = deployment
+        else:
+            raise WorkflowDefinitionException(
+                f"Checkpoint cannot use deployment {deployment} because it is not defined"
+            )
+        self.service = service
         self.copy_tasks: MutableSequence[asyncio.Task] = []
 
     async def _async_local_copy(self, data_location: DataLocation):
         parent_directory = os.path.join(self.checkpoint_dir, random_name())
         local_path = os.path.join(parent_directory, data_location.relpath)
-        # todo: add a parameter in StreamFlow file to indicate the deployment (local or remote) to store checkpoint data
-        dst_location = Location(deployment=LOCAL_LOCATION, name=LOCAL_LOCATION)
-
+        dst_location = list(
+            (
+                await self.context.deployment_manager.get_connector(
+                    self.deployment_name
+                ).get_available_locations(self.service)
+            ).values()
+        )[0]
         # writable is setted to True, in this way it is not created a symbolic link if file is available in the same location
         await self.context.data_manager.transfer_data(
             src_locations=[data_location],
