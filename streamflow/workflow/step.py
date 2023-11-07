a25a63e418913757e1813094d9bd86a6115cda76
Alberto Mulone <alberto.mulone@edu.unito.it>
2023-11-07 14:40:29 +0100

    fix: config is read-only so workflows can share the obj with same id

--- streamflow/workflow/step.py
+++ streamflow/workflow/step.py
@@ -1279,7 +1279,7 @@ class ScheduleStep(BaseStep):
             if change_wf
             else await loading_context.load_workflow(context, row["workflow"]),
             binding_config=await BindingConfig.load(
-                context, params["binding_config"], loading_context
+                context, params["binding_config"], loading_context, change_wf
             ),
             connector_ports={
                 k: cast(
