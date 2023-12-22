8ddc613da57f658a549ca3ba96bfc6cac4946310
Alberto Mulone <alberto.mulone@unito.it>
2023-12-22 20:34:38 +0100

    test

--- streamflow/data/manager.py
+++ streamflow/data/manager.py
@@ -170,10 +170,12 @@ class DefaultDataManager(DataManager):
         # Create destination folder
         await remotepath.mkdir(dst_connector, dst_locations, str(Path(dst_path).parent))
         # Follow symlink for source path
-        await src_location.available.wait()
-        src_path = await remotepath.follow_symlink(
-            self.context, src_connector, src_location, src_path
-        )
+        # if (src_location)
+        # await src_location.available.wait()
+        # a = self.path_mapper.get(src_path)
+        # src_path = await remotepath.follow_symlink(
+        #     self.context, src_connector, src_location, src_path
+        # )
         primary_locations = self.path_mapper.get(src_path, DataType.PRIMARY)
         copy_tasks = []
         remote_locations = []
