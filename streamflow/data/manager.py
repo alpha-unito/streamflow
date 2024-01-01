alpha-unito/streamflow

commit 1d0842a648ae506e2b730ec01737db166eeaa02d

Fixed symbolic link remote input data error (#328)
This commit fixes the case when input remote data involve a symbolic link
path. The path was saved in the StreamFlow filesystem as a regular file,
without any check. Now, the right file type is saved (`PRIMARY` for
regular files, `SYMBOLIC_LINK` for symbolic links). 
Plus, for symbolic links, also the primary path is saved.

Futhermore, this commit fixes a race condition in remote data transfers.
If a remote data object is required by two concurrent steps on the same
location and the two steps are executed in parallel, the first one starts
copying the object, while the second creates a symbolic link from the data
copied by the first one. However, the second must wait for the first one
to end the copy operation before creating the symbolic link.

Finally, this commit adds new in the logger to notify the end of copy
operations.

streamflow/data/manager.py
@@ -170,6 +170,14 @@ class DefaultDataManager(DataManager):
         # Create destination folder
         await remotepath.mkdir(dst_connector, dst_locations, str(Path(dst_path).parent))
         # Follow symlink for source path
+        await asyncio.gather(
+            *(
+                asyncio.create_task(src_data_loc.available.wait())
+                for src_data_loc in self.get_data_locations(
+                    src_path, src_location.deployment, src_location.name
+                )
+            )
+        )
         src_path = await remotepath.follow_symlink(
             self.context, src_connector, src_location, src_path
         )

streamflow/deployment/connector/base.py
@@ -275,6 +275,27 @@ class BaseConnector(Connector, FutureAware):
         await self._copy_local_to_remote(
             src=src, dst=dst, locations=locations, read_only=read_only
         )
+        if logger.isEnabledFor(logging.INFO):
+            if len(locations) > 1:
+                logger.info(
+                    "COMPLETED copy {src} on local file-system to {dst} on locations:\n\t{locations}".format(
+                        src=src,
+                        dst=dst,
+                        locations="\n\t".join([str(loc) for loc in locations]),
+                    )
+                )
+            else:
+                logger.info(
+                    "COMPLETED copy {src} on local file-system to {dst} {location}".format(
+                        src=src,
+                        dst=dst,
+                        location=(
+                            "on local file-system"
+                            if locations[0].name == LOCAL_LOCATION
+                            else f"on location {locations[0]}"
+                        ),
+                    )
+                )
 
     async def copy_remote_to_local(
         self,
@@ -292,6 +313,10 @@ class BaseConnector(Connector, FutureAware):
         await self._copy_remote_to_local(
             src=src, dst=dst, location=locations[0], read_only=read_only
         )
+        if logger.isEnabledFor(logging.INFO):
+            logger.info(
+                f"COMPLETED copy {src} on location {locations[0]} to {dst} on local file-system"
+            )
 
     async def copy_remote_to_remote(
         self,
@@ -329,6 +354,25 @@ class BaseConnector(Connector, FutureAware):
             source_connector=source_connector,
             read_only=read_only,
         )
+        if logger.isEnabledFor(logging.INFO):
+            if len(locations) > 1:
+                logger.info(
+                    "COMPLETED copy {src} on location {src_loc} to {dst} on locations:\n\t{locations}".format(
+                        src_loc=source_location,
+                        src=src,
+                        dst=dst,
+                        locations="\n\t".join([str(loc) for loc in locations]),
+                    )
+                )
+            else:
+                logger.info(
+                    "COMPLETED copy {src} on location {src_loc} to {dst} on location {location}".format(
+                        src_loc=source_location,
+                        src=src,
+                        dst=dst,
+                        location=locations[0],
+                    )
+                )
 
     async def run(
         self,

streamflow/workflow/step.py
@@ -19,6 +19,7 @@ from typing import (
 from streamflow.core import utils
 from streamflow.core.config import BindingConfig
 from streamflow.core.context import StreamFlowContext
+from streamflow.core.data import DataType
 from streamflow.core.deployment import Connector, DeploymentConfig, Location, Target
 from streamflow.core.exception import (
     FailureHandlingException,
@@ -1261,16 +1262,31 @@ class ScheduleStep(BaseStep):
         )
         # Register paths
         for location in locations:
-            for directory in [
+            for directory in (
                 job.input_directory,
                 job.output_directory,
                 job.tmp_directory,
-            ]:
-                self.workflow.context.data_manager.register_path(
-                    location=location,
-                    path=directory,
-                    relpath=directory,
-                )
+            ):
+                if not self.workflow.context.data_manager.get_data_locations(
+                    directory, location.deployment, location.name
+                ):
+                    realpath = await remotepath.follow_symlink(
+                        self.workflow.context, connector, location, directory
+                    )
+                    if realpath != directory:
+                        self.workflow.context.data_manager.register_path(
+                            location=location,
+                            path=realpath,
+                            relpath=realpath,
+                        )
+                    self.workflow.context.data_manager.register_path(
+                        location=location,
+                        path=directory,
+                        relpath=directory,
+                        data_type=DataType.PRIMARY
+                        if realpath == directory
+                        else DataType.SYMBOLIC_LINK,
+                    )
         # Propagate job
         token_inputs = []
         for step_port_name in self.input_ports.keys():

commit afbe1ffda0b937ac8313e5d96d1a85bb232ef2f5

Fixed `get` method in `RemotePathMapper` (#343)
This commit fixes the `get` method of the `RemotePathMapper` class
when trying to access a location not yet registered in the StreamFlow
filesystem. In particular, this commit fixes issue #342, probably caused by
a regression introduced by PR #328.

streamflow/data/manager.py
@@ -361,7 +361,7 @@ class RemotePathMapper:
         result = []
         for dep in [deployment] if deployment is not None else node.locations:
             for n in [name] if name is not None else node.locations.setdefault(dep, {}):
-                locations = node.locations[dep].setdefault(n, [])
+                locations = node.locations.setdefault(dep, {}).setdefault(n, [])
                 result.extend(
                     [
                         loc