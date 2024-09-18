alpha-unito/streamflow

commit 2a0f901bff955b54d131d5aa5666f2d39be58573

Improved description about location storage in the `Hardware` class (#423)
This commit enriches the location storage information in the `Hardware`
class. Before this commit, the input, output and temp directories of a `Job`
were managed as independent entities; instead, they should be correlated
when they are located in the same persistent volume. For example, if a step
requires 16 GB in the output directory and 8 GB in the temp directory and
the directories are in the same volume, the volume needs to have at least
24 GB of available storage.

Now, the attribute `storage` in the `Hardware` class maps each persistent
volume with a new object called `Storage`. The `Storage` class describes
the volume with a `mount_point` attribute, i.e., the path where the volume
is mounted in the related location. A `size` attribute contains the available
space in the volume, while a `paths` dictionary contains the set of visited
paths belonging to the target volume. 

---------

Co-authored-by: GlassOfWhiskey <iacopo.c92@gmail.com>

streamflow/core/scheduling.py
@@ -1,17 +1,41 @@
 from __future__ import annotations
 
+import os
 from abc import ABC, abstractmethod
-from typing import Any, TYPE_CHECKING, Type, cast
+from typing import TYPE_CHECKING
+
 
 from streamflow.core import utils
 from streamflow.core.config import BindingConfig, Config
 from streamflow.core.context import SchemaEntity, StreamFlowContext
 from streamflow.core.deployment import Connector, ExecutionLocation, Target
+from streamflow.core.exception import WorkflowExecutionException
 from streamflow.core.persistence import DatabaseLoadingContext
 
 if TYPE_CHECKING:
-    from streamflow.core.workflow import Job, Status, Token
-    from typing import MutableSequence, MutableMapping
+    from streamflow.core.workflow import Job, Status
+    from typing import (
+        Any,
+        Callable,
+        Iterable,
+        MutableMapping,
+        MutableSequence,
+        MutableSet,
+        Type,
+        cast,
+    )
+
+
+def _reduce_storages(
+    storages: Iterable[Storage], operator: Callable[[Storage, Any], Storage]
+) -> MutableMapping[str, Storage]:
+    storage = {}
+    for disk in storages:
+        if disk.mount_point in storage.keys():
+            storage[disk.mount_point] = operator(storage[disk.mount_point], disk)
+        else:
+            storage[disk.mount_point] = Storage(disk.mount_point, disk.size)
+    return storage
 
 
 class Hardware:
@@ -19,67 +43,123 @@ class Hardware:
         self,
         cores: float = 0.0,
         memory: float = 0.0,
-        input_directory: float = 0.0,
-        output_directory: float = 0.0,
-        tmp_directory: float = 0.0,
+        storage: MutableMapping[str, Storage] | None = None,
     ):
+        """
+        The `Hardware` class serves as a representation of the system's resources.
+
+        The `storage` attribute is a map with `key : storage`. The `key` meaning is left to the implementation of
+        the `HardwareRequirement`. No assumption should be made about any specific behaviour of the `key` field
+        outside this class. Only other classes of the same `HardwareRequirement` domain should be allowed to use
+        the `key` as a fast entry point to the `Storage`. When an operation (arithmetic or comparison) is done,
+        a new `Hardware` instance is created following a normalized form. In the normalized form, the `key` is equal
+        to the storage's mount point, and there is only one (aggregated) `Storage` object for each mount point.
+
+        :param cores: total number of cores
+        :param memory: total number of memory
+        :param storage: a map with string keys and `Storage` values
+        """
         self.cores: float = cores
         self.memory: float = memory
-        self.input_directory: float = input_directory
-        self.output_directory: float = output_directory
-        self.tmp_directory: float = tmp_directory
+        self.storage: MutableMapping[str, Storage] = storage or {
+            os.sep: Storage(os.sep, 0.0)
+        }
 
-    def __add__(self, other):
+    def _normalize_storage(self) -> MutableMapping[str, Storage]:
+        return _reduce_storages(self.storage.values(), Storage.__add__.__call__)
+
+    def get_mount_point(self, path: str) -> str:
+        return self.get_storage(path).mount_point
+
+    def get_mount_points(self) -> MutableSequence[str]:
+        return [storage.mount_point for storage in self.storage.values()]
+
+    def get_size(self, path: str) -> float:
+        return self.get_storage(path).size
+
+    def get_storage(self, path: str) -> Storage:
+        for disk in self.storage.values():
+            if path == disk.mount_point or path in disk.paths:
+                return disk
+        raise KeyError(path)
+
+    def __add__(self, other: Any) -> Hardware:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return self.__class__(
-            **{
-                k: vars(self).get(k, 0.0) + vars(other).get(k, 0.0)
-                for k in vars(self).keys()
-            }
+            raise NotImplementedError
+        return Hardware(
+            self.cores + other.cores,
+            self.memory + other.memory,
+            _reduce_storages(
+                (
+                    *self._normalize_storage().values(),
+                    *other._normalize_storage().values(),
+                ),
+                Storage.__add__.__call__,
+            ),
         )
 
-    def __sub__(self, other):
+    def __sub__(self, other: Any) -> Hardware:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return self.__class__(
-            **{
-                k: vars(self).get(k, 0.0) - vars(other).get(k, 0.0)
-                for k in vars(self).keys()
-            }
+            raise NotImplementedError
+        return Hardware(
+            self.cores - other.cores,
+            self.memory - other.memory,
+            _reduce_storages(
+                (
+                    *self._normalize_storage().values(),
+                    *other._normalize_storage().values(),
+                ),
+                Storage.__sub__.__call__,
+            ),
         )
 
-    def __ge__(self, other):
+    def __ge__(self, other: Any) -> bool:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return all(
-            vars(self).get(k, 0.0) >= vars(other).get(k, 0.0)
-            for k in set().union(vars(self).keys(), vars(other).keys())
-        )
+            raise NotImplementedError
+        if self.cores >= other.cores and self.memory >= other.memory:
+            normalized_storages = self._normalize_storage()
+            return all(
+                normalized_storages[disk.mount_point] >= disk
+                for disk in other._normalize_storage().values()
+            )
+        else:
+            return False
 
-    def __gt__(self, other):
+    def __gt__(self, other: Any) -> bool:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return all(
-            vars(self).get(k, 0.0) > vars(other).get(k, 0.0)
-            for k in set().union(vars(self).keys(), vars(other).keys())
-        )
+            raise NotImplementedError
+        if self.cores > other.cores and self.memory > other.memory:
+            normalized_storages = self._normalize_storage()
+            return all(
+                normalized_storages[disk.mount_point] > disk
+                for disk in other._normalize_storage().values()
+            )
+        else:
+            return False
 
-    def __le__(self, other):
+    def __le__(self, other: Any) -> bool:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return all(
-            vars(self).get(k, 0.0) <= vars(other).get(k, 0.0)
-            for k in set().union(vars(self).keys(), vars(other).keys())
-        )
+            raise NotImplementedError
+        if self.cores <= other.cores and self.memory <= other.memory:
+            normalized_storages = self._normalize_storage()
+            return all(
+                normalized_storages[disk.mount_point] <= disk
+                for disk in other._normalize_storage().values()
+            )
+        else:
+            return False
 
-    def __lt__(self, other):
+    def __lt__(self, other: Any) -> bool:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return all(
-            vars(self).get(k, 0.0) < vars(other).get(k, 0.0)
-            for k in set().union(vars(self).keys(), vars(other).keys())
-        )
+            raise NotImplementedError
+        if self.cores < other.cores and self.memory < other.memory:
+            normalized_storages = self._normalize_storage()
+            return all(
+                normalized_storages[disk.mount_point] < disk
+                for disk in other._normalize_storage().values()
+            )
+        else:
+            return False
 
 
 class HardwareRequirement(ABC):
@@ -98,7 +178,7 @@ class HardwareRequirement(ABC):
     ) -> MutableMapping[str, Any]: ...
 
     @abstractmethod
-    def eval(self, inputs: MutableMapping[str, Token]) -> Hardware: ...
+    def eval(self, job: Job) -> Hardware: ...
 
     @classmethod
     async def load(
@@ -262,5 +342,93 @@ class Scheduler(SchemaEntity):
 
     @abstractmethod
     async def schedule(
-        self, job: Job, binding_config: BindingConfig, hardware_requirement: Hardware
+        self,
+        job: Job,
+        binding_config: BindingConfig,
+        hardware_requirement: HardwareRequirement | None,
     ) -> None: ...
+
+
+class Storage:
+    def __init__(
+        self, mount_point: str, size: float, paths: MutableSet[str] | None = None
+    ):
+        """
+        The `Storage` class represents a persistent volume
+
+        :param mount_point: the path of the volume's mount point
+        :param size: the total size of the volume, expressed in Kilobyte
+        :param paths: a list of paths inside the volume
+        """
+        self.mount_point: str = mount_point
+        self.paths: MutableSet[str] = paths or set()
+        if size < 0:
+            raise WorkflowExecutionException(
+                f"Storage {mount_point} with {paths} paths cannot have negative size: {size}"
+            )
+        self.size: float = size
+
+    def add_path(self, path: str):
+        self.paths.add(path)
+
+    def __add__(self, other: Any) -> Storage:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise WorkflowExecutionException(
+                f"Cannot sum two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return Storage(
+            mount_point=self.mount_point,
+            size=self.size + other.size,
+            paths=self.paths | other.paths,
+        )
+
+    def __sub__(self, other: Any) -> Storage:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise WorkflowExecutionException(
+                f"Cannot subtract two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return Storage(
+            mount_point=self.mount_point,
+            size=self.size - other.size,
+            paths=self.paths | other.paths,
+        )
+
+    def __ge__(self, other: Storage) -> bool:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise KeyError(
+                f"Cannot compare two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return self.size >= other.size
+
+    def __gt__(self, other: Storage) -> bool:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise KeyError(
+                f"Cannot compare two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return self.size > other.size
+
+    def __le__(self, other: Storage) -> bool:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise KeyError(
+                f"Cannot compare two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return self.size <= other.size
+
+    def __lt__(self, other: Storage) -> bool:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise KeyError(
+                f"Cannot compare two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return self.size < other.size

commit 73542e2cca7295ed8215583d338672663d45dc73

This PR improves some minor bugs - fixed a regression introduced in #423 which the loading of the `HardwareRequirement` failed for erroneous package import. Now, it was fixed and a new test is added. - fixed some types in the signatures - replaced direct access to the job_allocation from scheduler with the method calling

streamflow/core/persistence.py
@@ -14,46 +14,62 @@ if TYPE_CHECKING:
 
 class DatabaseLoadingContext(ABC):
     @abstractmethod
-    def add_deployment(self, persistent_id: int, deployment: DeploymentConfig): ...
+    def add_deployment(
+        self, persistent_id: int, deployment: DeploymentConfig
+    ) -> None: ...
 
     @abstractmethod
-    def add_filter(self, persistent_id: int, filter_config: FilterConfig): ...
+    def add_filter(self, persistent_id: int, filter_config: FilterConfig) -> None: ...
 
     @abstractmethod
-    def add_port(self, persistent_id: int, port: Port): ...
+    def add_port(self, persistent_id: int, port: Port) -> None: ...
 
     @abstractmethod
-    def add_step(self, persistent_id: int, step: Step): ...
+    def add_step(self, persistent_id: int, step: Step) -> None: ...
 
     @abstractmethod
-    def add_target(self, persistent_id: int, target: Target): ...
+    def add_target(self, persistent_id: int, target: Target) -> None: ...
 
     @abstractmethod
-    def add_token(self, persistent_id: int, token: Token): ...
+    def add_token(self, persistent_id: int, token: Token) -> None: ...
 
     @abstractmethod
-    def add_workflow(self, persistent_id: int, workflow: Workflow): ...
+    def add_workflow(self, persistent_id: int, workflow: Workflow) -> None: ...
 
     @abstractmethod
-    async def load_deployment(self, context: StreamFlowContext, persistent_id: int): ...
+    async def load_deployment(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> DeploymentConfig: ...
 
     @abstractmethod
-    async def load_filter(self, context: StreamFlowContext, persistent_id: int): ...
+    async def load_filter(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> FilterConfig: ...
 
     @abstractmethod
-    async def load_port(self, context: StreamFlowContext, persistent_id: int): ...
+    async def load_port(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> Port: ...
 
     @abstractmethod
-    async def load_step(self, context: StreamFlowContext, persistent_id: int): ...
+    async def load_step(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> Step: ...
 
     @abstractmethod
-    async def load_target(self, context: StreamFlowContext, persistent_id: int): ...
+    async def load_target(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> Target: ...
 
     @abstractmethod
-    async def load_token(self, context: StreamFlowContext, persistent_id: int): ...
+    async def load_token(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> Token: ...
 
     @abstractmethod
-    async def load_workflow(self, context: StreamFlowContext, persistent_id: int): ...
+    async def load_workflow(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> Workflow: ...
 
 
 class PersistableEntity:

streamflow/core/scheduling.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import os
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Type, cast
 
 
 from streamflow.core import utils
@@ -21,8 +21,6 @@ if TYPE_CHECKING:
         MutableMapping,
         MutableSequence,
         MutableSet,
-        Type,
-        cast,
     )
 
 
@@ -397,7 +395,7 @@ class Storage:
             paths=self.paths | other.paths,
         )
 
-    def __ge__(self, other: Storage) -> bool:
+    def __ge__(self, other: Any) -> bool:
         if not isinstance(other, Storage):
             raise NotImplementedError
         if self.mount_point != other.mount_point:
@@ -406,7 +404,7 @@ class Storage:
             )
         return self.size >= other.size
 
-    def __gt__(self, other: Storage) -> bool:
+    def __gt__(self, other: Any) -> bool:
         if not isinstance(other, Storage):
             raise NotImplementedError
         if self.mount_point != other.mount_point:
@@ -415,7 +413,7 @@ class Storage:
             )
         return self.size > other.size
 
-    def __le__(self, other: Storage) -> bool:
+    def __le__(self, other: Any) -> bool:
         if not isinstance(other, Storage):
             raise NotImplementedError
         if self.mount_point != other.mount_point:
@@ -424,7 +422,7 @@ class Storage:
             )
         return self.size <= other.size
 
-    def __lt__(self, other: Storage) -> bool:
+    def __lt__(self, other: Any) -> bool:
         if not isinstance(other, Storage):
             raise NotImplementedError
         if self.mount_point != other.mount_point:

streamflow/persistence/loading_context.py
@@ -19,65 +19,73 @@ class DefaultDatabaseLoadingContext(DatabaseLoadingContext):
         self._tokens: MutableMapping[int, Token] = {}
         self._workflows: MutableMapping[int, Workflow] = {}
 
-    def add_deployment(self, persistent_id: int, deployment: DeploymentConfig):
+    def add_deployment(self, persistent_id: int, deployment: DeploymentConfig) -> None:
         deployment.persistent_id = persistent_id
         self._deployment_configs[persistent_id] = deployment
 
-    def add_filter(self, persistent_id: int, filter_config: FilterConfig):
+    def add_filter(self, persistent_id: int, filter_config: FilterConfig) -> None:
         filter_config.persistent_id = persistent_id
         self._filter_configs[persistent_id] = filter_config
 
-    def add_port(self, persistent_id: int, port: Port):
+    def add_port(self, persistent_id: int, port: Port) -> None:
         port.persistent_id = persistent_id
         self._ports[persistent_id] = port
 
-    def add_step(self, persistent_id: int, step: Step):
+    def add_step(self, persistent_id: int, step: Step) -> None:
         step.persistent_id = persistent_id
         self._steps[persistent_id] = step
 
-    def add_target(self, persistent_id: int, target: Target):
+    def add_target(self, persistent_id: int, target: Target) -> None:
         target.persistent_id = persistent_id
         self._targets[persistent_id] = target
 
-    def add_token(self, persistent_id: int, token: Token):
+    def add_token(self, persistent_id: int, token: Token) -> None:
         token.persistent_id = persistent_id
         self._tokens[persistent_id] = token
 
-    def add_workflow(self, persistent_id: int, workflow: Workflow):
+    def add_workflow(self, persistent_id: int, workflow: Workflow) -> None:
         workflow.persistent_id = persistent_id
         self._workflows[persistent_id] = workflow
 
-    async def load_deployment(self, context: StreamFlowContext, persistent_id: int):
+    async def load_deployment(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> DeploymentConfig:
         return self._deployment_configs.get(
             persistent_id
         ) or await DeploymentConfig.load(context, persistent_id, self)
 
-    async def load_filter(self, context: StreamFlowContext, persistent_id: int):
+    async def load_filter(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> FilterConfig:
         return self._filter_configs.get(persistent_id) or await FilterConfig.load(
             context, persistent_id, self
         )
 
-    async def load_port(self, context: StreamFlowContext, persistent_id: int):
+    async def load_port(self, context: StreamFlowContext, persistent_id: int) -> Port:
         return self._ports.get(persistent_id) or await Port.load(
             context, persistent_id, self
         )
 
-    async def load_step(self, context: StreamFlowContext, persistent_id: int):
+    async def load_step(self, context: StreamFlowContext, persistent_id: int) -> Step:
         return self._steps.get(persistent_id) or await Step.load(
             context, persistent_id, self
         )
 
-    async def load_target(self, context: StreamFlowContext, persistent_id: int):
+    async def load_target(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> Target:
         return self._targets.get(persistent_id) or await Target.load(
             context, persistent_id, self
         )
 
-    async def load_token(self, context: StreamFlowContext, persistent_id: int):
+    async def load_token(self, context: StreamFlowContext, persistent_id: int) -> Token:
         return self._tokens.get(persistent_id) or await Token.load(
             context, persistent_id, self
         )
 
-    async def load_workflow(self, context: StreamFlowContext, persistent_id: int):
+    async def load_workflow(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> Workflow:
         return self._workflows.get(persistent_id) or await Workflow.load(
             context, persistent_id, self
         )
@@ -88,16 +96,28 @@ class WorkflowBuilder(DefaultDatabaseLoadingContext):
         super().__init__()
         self.workflow: Workflow = workflow
 
-    def add_port(self, persistent_id: int, port: Port):
+    def add_port(self, persistent_id: int, port: Port) -> None:
         self._ports[persistent_id] = port
 
-    def add_step(self, persistent_id: int, step: Step):
+    def add_step(self, persistent_id: int, step: Step) -> None:
         self._steps[persistent_id] = step
 
-    def add_workflow(self, persistent_id: int, workflow: Workflow):
+    def add_workflow(self, persistent_id: int, workflow: Workflow) -> None:
         self._workflows[persistent_id] = self.workflow
 
-    async def load_step(self, context: StreamFlowContext, persistent_id: int):
+    async def load_port(self, context: StreamFlowContext, persistent_id: int) -> Port:
+        if persistent_id in self._ports.keys():
+            return self._ports[persistent_id]
+        else:
+            port_row = await context.database.get_port(persistent_id)
+            if (port := self.workflow.ports.get(port_row["name"])) is None:
+                # If the port is not available in the new workflow, a new one must be created
+                self.add_workflow(port_row["workflow"], self.workflow)
+                port = await Port.load(context, persistent_id, self)
+                self.workflow.ports[port.name] = port
+            return port
+
+    async def load_step(self, context: StreamFlowContext, persistent_id: int) -> Step:
         if persistent_id in self._steps.keys():
             return self._steps[persistent_id]
         else:
@@ -114,19 +134,9 @@ class WorkflowBuilder(DefaultDatabaseLoadingContext):
                 self.workflow.steps[step.name] = step
             return step
 
-    async def load_port(self, context: StreamFlowContext, persistent_id: int):
-        if persistent_id in self._ports.keys():
-            return self._ports[persistent_id]
-        else:
-            port_row = await context.database.get_port(persistent_id)
-            if (port := self.workflow.ports.get(port_row["name"])) is None:
-                # If the port is not available in the new workflow, a new one must be created
-                self.add_workflow(port_row["workflow"], self.workflow)
-                port = await Port.load(context, persistent_id, self)
-                self.workflow.ports[port.name] = port
-            return port
-
-    async def load_workflow(self, context: StreamFlowContext, persistent_id: int):
+    async def load_workflow(
+        self, context: StreamFlowContext, persistent_id: int
+    ) -> Workflow:
         if persistent_id not in self._workflows.keys():
             await Workflow.load(context, persistent_id, self)
         return self.workflow

tests/test_cwl_persistence.py
@@ -21,6 +21,7 @@ from streamflow.cwl.command import (
     CWLMapCommandTokenProcessor,
     CWLObjectCommandTokenProcessor,
 )
+from streamflow.cwl.hardware import CWLHardwareRequirement
 from streamflow.cwl.processor import (
     CWLFileToken,
     CWLMapTokenProcessor,
@@ -727,7 +728,7 @@ async def test_cwl_loop_output(context: StreamFlowContext, step_cls: type[Step])
 
 @pytest.mark.asyncio
 async def test_cwl_schedule_step(context: StreamFlowContext):
-    """Test saving and loading CWLScheduleStep from database"""
+    """Test saving and loading CWLScheduleStep with a CWLHardwareRequirement from database"""
     workflow, _ = await create_workflow(context, 0)
     binding_config = BindingConfig(
         targets=[
@@ -750,6 +751,15 @@ async def test_cwl_schedule_step(context: StreamFlowContext):
         output_directory=posixpath.join(*(utils.random_name() for _ in range(2))),
         tmp_directory=posixpath.join(*(utils.random_name() for _ in range(2))),
         binding_config=binding_config,
+        hardware_requirement=CWLHardwareRequirement(
+            CWL_VERSION,
+            1.5,
+            "$(inputs.mem * 2)",
+            1024,
+            4096,
+            True,
+            ["function function nope(message) {return message}"],
+        ),
     )
     await save_load_and_test(schedule_step, context)
 

tests/test_scheduler.py
@@ -56,9 +56,9 @@ async def test_scheduling(
     target = Target(deployment=deployment_config, service=service)
     binding_config = BindingConfig(targets=[target])
     await context.scheduler.schedule(job, binding_config, hardware_requirement)
-    assert context.scheduler.job_allocations[job.name].status == Status.FIREABLE
+    assert context.scheduler.get_allocation(job.name).status == Status.FIREABLE
     await context.scheduler.notify_status(job.name, Status.COMPLETED)
-    assert context.scheduler.job_allocations[job.name].status == Status.COMPLETED
+    assert context.scheduler.get_allocation(job.name).status == Status.COMPLETED
 
 
 @pytest.mark.asyncio
@@ -341,21 +341,21 @@ async def test_multi_targets_one_job(context: StreamFlowContext):
         task_pending, return_when=asyncio.FIRST_COMPLETED, timeout=60
     )
     assert len(task_pending) == 0
-    assert context.scheduler.job_allocations[job.name].status == Status.FIREABLE
+    assert context.scheduler.get_allocation(job.name).status == Status.FIREABLE
 
     # Check if it has been scheduled into the first target
     assert (
-        context.scheduler.job_allocations[job.name].target.deployment.name
+        context.scheduler.get_allocation(job.name).target.deployment.name
         == param_config.name
     )
 
     # Job changes status to RUNNING
     await context.scheduler.notify_status(job.name, Status.RUNNING)
-    assert context.scheduler.job_allocations[job.name].status == Status.RUNNING
+    assert context.scheduler.get_allocation(job.name).status == Status.RUNNING
 
     # Job changes status to COMPLETED
     await context.scheduler.notify_status(job.name, Status.COMPLETED)
-    assert context.scheduler.job_allocations[job.name].status == Status.COMPLETED
+    assert context.scheduler.get_allocation(job.name).status == Status.COMPLETED
 
 
 @pytest.mark.asyncio
@@ -480,18 +480,18 @@ async def test_binding_filter(context: StreamFlowContext):
         task_pending, return_when=asyncio.FIRST_COMPLETED, timeout=60
     )
     assert len(task_pending) == 0
-    assert context.scheduler.job_allocations[job.name].status == Status.FIREABLE
+    assert context.scheduler.get_allocation(job.name).status == Status.FIREABLE
 
     # Check if the job has been scheduled into the second target
     assert (
-        context.scheduler.job_allocations[job.name].target.deployment.name
+        context.scheduler.get_allocation(job.name).target.deployment.name
         == docker_config.name
     )
 
     # Job changes status to RUNNING
     await context.scheduler.notify_status(job.name, Status.RUNNING)
-    assert context.scheduler.job_allocations[job.name].status == Status.RUNNING
+    assert context.scheduler.get_allocation(job.name).status == Status.RUNNING
 
     # Job changes status to COMPLETED
     await context.scheduler.notify_status(job.name, Status.COMPLETED)
-    assert context.scheduler.job_allocations[job.name].status == Status.COMPLETED
+    assert context.scheduler.get_allocation(job.name).status == Status.COMPLETED