alpha-unito/streamflow

commit 2a0f901bff955b54d131d5aa5666f2d39be58573

Improved description about location storage in the `Hardware` class (#423)
This commit enriches the location storage information in the `Hardware`
class. Before this commit, the input, output and temp directories of a `Job`
were managed as independent entities; instead, they should be correlated
when they are located in the same persistent volume. For example, if a step
requires 16 GB in the output directory and 8 GB in the temp directory and
the directories are in the same volume, the volume needs to have at least
24 GB of available storage.

Now, the attribute `storage` in the `Hardware` class maps each persistent
volume with a new object called `Storage`. The `Storage` class describes
the volume with a `mount_point` attribute, i.e., the path where the volume
is mounted in the related location. A `size` attribute contains the available
space in the volume, while a `paths` dictionary contains the set of visited
paths belonging to the target volume. 

---------

Co-authored-by: GlassOfWhiskey <iacopo.c92@gmail.com>

streamflow/cwl/translator.py
@@ -79,6 +79,7 @@ from streamflow.cwl.step import (
     CWLLoopOutputAllStep,
     CWLLoopOutputLastStep,
     CWLTransferStep,
+    CWLScheduleStep,
 )
 from streamflow.cwl.transformer import (
     AllNonNullTransformer,
@@ -1426,9 +1427,6 @@ class CWLTranslator:
             name=posixpath.join(f"{global_name}-injector", "__schedule__"),
             job_prefix=f"{global_name}-injector",
             connector_ports={target.deployment.name: deploy_step.get_output_port()},
-            input_directory=target.workdir or output_directory,
-            output_directory=target.workdir or output_directory,
-            tmp_directory=target.workdir or output_directory,
             binding_config=binding_config,
         )
         # Create a CWLInputInjector step to process the input
@@ -1595,7 +1593,7 @@ class CWLTranslator:
         }
         # Create a schedule step and connect it to the DeployStep
         schedule_step = workflow.create_step(
-            cls=ScheduleStep,
+            cls=CWLScheduleStep,
             name=posixpath.join(name_prefix, "__schedule__"),
             job_prefix=name_prefix,
             connector_ports={
@@ -2474,9 +2472,6 @@ class CWLTranslator:
                 ),
                 job_prefix=f"{global_name}-value-from",
                 connector_ports={target.deployment.name: deploy_step.get_output_port()},
-                input_directory=target.workdir or self.output_directory,
-                output_directory=target.workdir or self.output_directory,
-                tmp_directory=target.workdir or self.output_directory,
                 binding_config=binding_config,
             )
 

.github/workflows/ci-tests.yaml
@@ -147,7 +147,7 @@ jobs:
           python -m pip install -r docs/requirements.txt
       - name: "Build documentation and check for consistency"
         env:
-          CHECKSUM: "1b1ab1a8783b04e85f248d71033ac0fbe6acf1eba6dd80bf633993e8ea451798"
+          CHECKSUM: "e0cf087fb23ba972b358e25a62f52619c4a16478e1f5a703cc073a8775408c4e"
         run: |
           cd docs
           HASH="$(make checksum | tail -n1)"

streamflow/core/deployment.py
@@ -104,11 +104,7 @@ class Connector(SchemaEntity):
 
     @abstractmethod
     async def get_available_locations(
-        self,
-        service: str | None = None,
-        input_directory: str | None = None,
-        output_directory: str | None = None,
-        tmp_directory: str | None = None,
+        self, service: str | None = None
     ) -> MutableMapping[str, AvailableLocation]: ...
 
     @abstractmethod

streamflow/core/scheduling.py
@@ -1,17 +1,41 @@
 from __future__ import annotations
 
+import os
 from abc import ABC, abstractmethod
-from typing import Any, TYPE_CHECKING, Type, cast
+from typing import TYPE_CHECKING
+
 
 from streamflow.core import utils
 from streamflow.core.config import BindingConfig, Config
 from streamflow.core.context import SchemaEntity, StreamFlowContext
 from streamflow.core.deployment import Connector, ExecutionLocation, Target
+from streamflow.core.exception import WorkflowExecutionException
 from streamflow.core.persistence import DatabaseLoadingContext
 
 if TYPE_CHECKING:
-    from streamflow.core.workflow import Job, Status, Token
-    from typing import MutableSequence, MutableMapping
+    from streamflow.core.workflow import Job, Status
+    from typing import (
+        Any,
+        Callable,
+        Iterable,
+        MutableMapping,
+        MutableSequence,
+        MutableSet,
+        Type,
+        cast,
+    )
+
+
+def _reduce_storages(
+    storages: Iterable[Storage], operator: Callable[[Storage, Any], Storage]
+) -> MutableMapping[str, Storage]:
+    storage = {}
+    for disk in storages:
+        if disk.mount_point in storage.keys():
+            storage[disk.mount_point] = operator(storage[disk.mount_point], disk)
+        else:
+            storage[disk.mount_point] = Storage(disk.mount_point, disk.size)
+    return storage
 
 
 class Hardware:
@@ -19,67 +43,123 @@ class Hardware:
         self,
         cores: float = 0.0,
         memory: float = 0.0,
-        input_directory: float = 0.0,
-        output_directory: float = 0.0,
-        tmp_directory: float = 0.0,
+        storage: MutableMapping[str, Storage] | None = None,
     ):
+        """
+        The `Hardware` class serves as a representation of the system's resources.
+
+        The `storage` attribute is a map with `key : storage`. The `key` meaning is left to the implementation of
+        the `HardwareRequirement`. No assumption should be made about any specific behaviour of the `key` field
+        outside this class. Only other classes of the same `HardwareRequirement` domain should be allowed to use
+        the `key` as a fast entry point to the `Storage`. When an operation (arithmetic or comparison) is done,
+        a new `Hardware` instance is created following a normalized form. In the normalized form, the `key` is equal
+        to the storage's mount point, and there is only one (aggregated) `Storage` object for each mount point.
+
+        :param cores: total number of cores
+        :param memory: total number of memory
+        :param storage: a map with string keys and `Storage` values
+        """
         self.cores: float = cores
         self.memory: float = memory
-        self.input_directory: float = input_directory
-        self.output_directory: float = output_directory
-        self.tmp_directory: float = tmp_directory
+        self.storage: MutableMapping[str, Storage] = storage or {
+            os.sep: Storage(os.sep, 0.0)
+        }
 
-    def __add__(self, other):
+    def _normalize_storage(self) -> MutableMapping[str, Storage]:
+        return _reduce_storages(self.storage.values(), Storage.__add__.__call__)
+
+    def get_mount_point(self, path: str) -> str:
+        return self.get_storage(path).mount_point
+
+    def get_mount_points(self) -> MutableSequence[str]:
+        return [storage.mount_point for storage in self.storage.values()]
+
+    def get_size(self, path: str) -> float:
+        return self.get_storage(path).size
+
+    def get_storage(self, path: str) -> Storage:
+        for disk in self.storage.values():
+            if path == disk.mount_point or path in disk.paths:
+                return disk
+        raise KeyError(path)
+
+    def __add__(self, other: Any) -> Hardware:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return self.__class__(
-            **{
-                k: vars(self).get(k, 0.0) + vars(other).get(k, 0.0)
-                for k in vars(self).keys()
-            }
+            raise NotImplementedError
+        return Hardware(
+            self.cores + other.cores,
+            self.memory + other.memory,
+            _reduce_storages(
+                (
+                    *self._normalize_storage().values(),
+                    *other._normalize_storage().values(),
+                ),
+                Storage.__add__.__call__,
+            ),
         )
 
-    def __sub__(self, other):
+    def __sub__(self, other: Any) -> Hardware:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return self.__class__(
-            **{
-                k: vars(self).get(k, 0.0) - vars(other).get(k, 0.0)
-                for k in vars(self).keys()
-            }
+            raise NotImplementedError
+        return Hardware(
+            self.cores - other.cores,
+            self.memory - other.memory,
+            _reduce_storages(
+                (
+                    *self._normalize_storage().values(),
+                    *other._normalize_storage().values(),
+                ),
+                Storage.__sub__.__call__,
+            ),
         )
 
-    def __ge__(self, other):
+    def __ge__(self, other: Any) -> bool:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return all(
-            vars(self).get(k, 0.0) >= vars(other).get(k, 0.0)
-            for k in set().union(vars(self).keys(), vars(other).keys())
-        )
+            raise NotImplementedError
+        if self.cores >= other.cores and self.memory >= other.memory:
+            normalized_storages = self._normalize_storage()
+            return all(
+                normalized_storages[disk.mount_point] >= disk
+                for disk in other._normalize_storage().values()
+            )
+        else:
+            return False
 
-    def __gt__(self, other):
+    def __gt__(self, other: Any) -> bool:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return all(
-            vars(self).get(k, 0.0) > vars(other).get(k, 0.0)
-            for k in set().union(vars(self).keys(), vars(other).keys())
-        )
+            raise NotImplementedError
+        if self.cores > other.cores and self.memory > other.memory:
+            normalized_storages = self._normalize_storage()
+            return all(
+                normalized_storages[disk.mount_point] > disk
+                for disk in other._normalize_storage().values()
+            )
+        else:
+            return False
 
-    def __le__(self, other):
+    def __le__(self, other: Any) -> bool:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return all(
-            vars(self).get(k, 0.0) <= vars(other).get(k, 0.0)
-            for k in set().union(vars(self).keys(), vars(other).keys())
-        )
+            raise NotImplementedError
+        if self.cores <= other.cores and self.memory <= other.memory:
+            normalized_storages = self._normalize_storage()
+            return all(
+                normalized_storages[disk.mount_point] <= disk
+                for disk in other._normalize_storage().values()
+            )
+        else:
+            return False
 
-    def __lt__(self, other):
+    def __lt__(self, other: Any) -> bool:
         if not isinstance(other, Hardware):
-            return NotImplementedError
-        return all(
-            vars(self).get(k, 0.0) < vars(other).get(k, 0.0)
-            for k in set().union(vars(self).keys(), vars(other).keys())
-        )
+            raise NotImplementedError
+        if self.cores < other.cores and self.memory < other.memory:
+            normalized_storages = self._normalize_storage()
+            return all(
+                normalized_storages[disk.mount_point] < disk
+                for disk in other._normalize_storage().values()
+            )
+        else:
+            return False
 
 
 class HardwareRequirement(ABC):
@@ -98,7 +178,7 @@ class HardwareRequirement(ABC):
     ) -> MutableMapping[str, Any]: ...
 
     @abstractmethod
-    def eval(self, inputs: MutableMapping[str, Token]) -> Hardware: ...
+    def eval(self, job: Job) -> Hardware: ...
 
     @classmethod
     async def load(
@@ -262,5 +342,93 @@ class Scheduler(SchemaEntity):
 
     @abstractmethod
     async def schedule(
-        self, job: Job, binding_config: BindingConfig, hardware_requirement: Hardware
+        self,
+        job: Job,
+        binding_config: BindingConfig,
+        hardware_requirement: HardwareRequirement | None,
     ) -> None: ...
+
+
+class Storage:
+    def __init__(
+        self, mount_point: str, size: float, paths: MutableSet[str] | None = None
+    ):
+        """
+        The `Storage` class represents a persistent volume
+
+        :param mount_point: the path of the volume's mount point
+        :param size: the total size of the volume, expressed in Kilobyte
+        :param paths: a list of paths inside the volume
+        """
+        self.mount_point: str = mount_point
+        self.paths: MutableSet[str] = paths or set()
+        if size < 0:
+            raise WorkflowExecutionException(
+                f"Storage {mount_point} with {paths} paths cannot have negative size: {size}"
+            )
+        self.size: float = size
+
+    def add_path(self, path: str):
+        self.paths.add(path)
+
+    def __add__(self, other: Any) -> Storage:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise WorkflowExecutionException(
+                f"Cannot sum two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return Storage(
+            mount_point=self.mount_point,
+            size=self.size + other.size,
+            paths=self.paths | other.paths,
+        )
+
+    def __sub__(self, other: Any) -> Storage:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise WorkflowExecutionException(
+                f"Cannot subtract two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return Storage(
+            mount_point=self.mount_point,
+            size=self.size - other.size,
+            paths=self.paths | other.paths,
+        )
+
+    def __ge__(self, other: Storage) -> bool:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise KeyError(
+                f"Cannot compare two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return self.size >= other.size
+
+    def __gt__(self, other: Storage) -> bool:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise KeyError(
+                f"Cannot compare two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return self.size > other.size
+
+    def __le__(self, other: Storage) -> bool:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise KeyError(
+                f"Cannot compare two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return self.size <= other.size
+
+    def __lt__(self, other: Storage) -> bool:
+        if not isinstance(other, Storage):
+            raise NotImplementedError
+        if self.mount_point != other.mount_point:
+            raise KeyError(
+                f"Cannot compare two storages with different mount points: {self.mount_point} and {other.mount_point}"
+            )
+        return self.size < other.size

streamflow/core/utils.py
@@ -251,7 +251,7 @@ async def get_remote_to_remote_write_command(
             return ["tar", "xf", "-", "-C", posixpath.dirname(dst)]
 
 
-def get_size(path):
+def get_size(path: str) -> int:
     if os.path.isfile(path):
         return os.path.getsize(path)
     else:

streamflow/cwl/hardware.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
 import math
-from typing import Any, MutableMapping, MutableSequence
+import os
+from typing import Any, MutableMapping, MutableSequence, TYPE_CHECKING
 
 from streamflow.core.context import StreamFlowContext
 from streamflow.core.persistence import DatabaseLoadingContext
-from streamflow.core.scheduling import Hardware, HardwareRequirement
-from streamflow.core.workflow import Token
+from streamflow.core.scheduling import Hardware, HardwareRequirement, Storage
 from streamflow.cwl.utils import eval_expression
 from streamflow.workflow.utils import get_token_value
+
+if TYPE_CHECKING:
+    from streamflow.core.workflow import Job
 
 
 class CWLHardwareRequirement(HardwareRequirement):
@@ -70,11 +73,31 @@ class CWLHardwareRequirement(HardwareRequirement):
             )
         )
 
-    def eval(self, inputs: MutableMapping[str, Token]) -> Hardware:
-        context = {"inputs": {name: get_token_value(t) for name, t in inputs.items()}}
+    def get_cores(self, context):
+        return self._process_requirement(self.cores, context)
+
+    def get_memory(self, context):
+        return self._process_requirement(self.memory, context)
+
+    def get_outdir_size(self, context):
+        return self._process_requirement(self.outdir, context)
+
+    def get_tmpdir_size(self, context):
+        return self._process_requirement(self.tmpdir, context)
+
+    def eval(self, job: Job) -> Hardware:
+        context = {
+            "inputs": {name: get_token_value(t) for name, t in job.inputs.items()}
+        }
         return Hardware(
-            cores=self._process_requirement(self.cores, context),
-            memory=self._process_requirement(self.memory, context),
-            tmp_directory=self._process_requirement(self.tmpdir, context),
-            output_directory=self._process_requirement(self.outdir, context),
+            cores=self.get_cores(context),
+            memory=self.get_memory(context),
+            storage={
+                "__outdir__": Storage(
+                    os.sep, self.get_outdir_size(context), {job.output_directory}
+                ),
+                "__tmpdir__": Storage(
+                    os.sep, self.get_tmpdir_size(context), {job.tmp_directory}
+                ),
+            },
         )

streamflow/cwl/step.py
@@ -9,6 +9,7 @@ from typing import Any, MutableMapping, MutableSequence, cast
 
 from streamflow.core.context import StreamFlowContext
 from streamflow.core.data import DataType
+from streamflow.core.deployment import Connector, ExecutionLocation
 from streamflow.core.exception import (
     WorkflowDefinitionException,
     WorkflowExecutionException,
@@ -36,6 +37,7 @@ from streamflow.workflow.step import (
     LoopOutputStep,
     TransferStep,
     _get_token_ids,
+    ScheduleStep,
 )
 from streamflow.workflow.token import IterationTerminationToken, ListToken, ObjectToken
 
@@ -405,6 +407,19 @@ class CWLLoopOutputLastStep(LoopOutputStep):
                 key=lambda t: int(t.tag.split(".")[-1]),
             )[-1],
         )
+
+
+class CWLScheduleStep(ScheduleStep):
+    async def _set_job_directories(
+        self,
+        connector: Connector,
+        locations: MutableSequence[ExecutionLocation],
+        job: Job,
+    ):
+        await super()._set_job_directories(connector, locations, job)
+        hardware = self.workflow.context.scheduler.get_hardware(job.name)
+        hardware.storage["__outdir__"].add_path(job.output_directory)
+        hardware.storage["__tmpdir__"].add_path(job.tmp_directory)
 
 
 class CWLTransferStep(TransferStep):

streamflow/cwl/utils.py
@@ -8,12 +8,7 @@ import urllib.parse
 from enum import Enum
 from pathlib import PurePath
 from types import ModuleType
-from typing import (
-    Any,
-    MutableMapping,
-    MutableSequence,
-    cast,
-)
+from typing import Any, MutableMapping, MutableSequence, cast
 
 import cwl_utils.expression
 import cwltool.context
@@ -251,10 +246,8 @@ def build_context(
     if hardware:
         context["runtime"]["cores"] = hardware.cores
         context["runtime"]["ram"] = hardware.memory
-        # noinspection PyUnresolvedReferences
-        context["runtime"]["tmpdirSize"] = hardware.tmp_directory
-        # noinspection PyUnresolvedReferences
-        context["runtime"]["outdirSize"] = hardware.output_directory
+        context["runtime"]["tmpdirSize"] = hardware.storage["__tmpdir__"].size
+        context["runtime"]["outdirSize"] = hardware.storage["__outdir__"].size
     return context
 
 

commit 4d9f476ce13d12655078ba944783d966b71122ae

This commit fixes an issue introduced in #423. This fix allows to have initial workflow data located in remote location. In the StreamFlow file, it is possible to bind the ports with a location and define the workdir of the ports. The workflow config file (e.g. the CWL config file) define the relative path based on the workdir defined in the StreamFlow file.

streamflow/cwl/translator.py
@@ -1475,6 +1475,9 @@ class CWLTranslator:
             job_prefix=f"{global_name}-injector",
             connector_ports={target.deployment.name: deploy_step.get_output_port()},
             binding_config=binding_config,
+            input_directory=target.workdir or self.output_directory,
+            output_directory=target.workdir or self.output_directory,
+            tmp_directory=target.workdir or self.output_directory,
         )
         # Create a CWLInputInjector step to process the input
         injector_step = workflow.create_step(