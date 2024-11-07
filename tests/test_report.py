b89a29ed5411eefda6ad773f70872df549f8313c
LanderOtto <alberto.mulone@unito.it>
2024-11-07 09:23:27 +0100

    Removed png and webp checksum

--- tests/test_report.py
+++ tests/test_report.py
@@ -37,8 +37,6 @@ async def test_single_workflow_single_execution(
     digests = {
         "csv": "05ea34f00c0351d7812395abb175b6f3a98e131c4fcbc82053a940a853e08582",
         "json": "992e2d10f1b0c5ed1bac5574cad607389f675522e993cf80719fc9995c2c2912",
-        "png": "928b5220647d7983496983976bff1108c14f9ad72a7d416daec96d08b8ec3cfc",
-        "webp": "6741cb4f0df92689d54d29b4553074a953c179b0763948d5bfb1c5adc6d2394d",
     }
     with TemporaryDirectory() as tmpdir:
         await create_report(
@@ -65,8 +63,6 @@ async def test_single_workflow_all_instances(context: StreamFlowContext) -> None
     digests = {
         "csv": "aeb96671257cf1ec501e8c33b568d69c98948a7cd72e1b8c048015a7faf71054",
         "json": "fc8261cf1df845a12e8738ff115773e0f3f73f38631c03942bf49830afb9c423",
-        "png": "e2559a28723ef090123dca1dcb87599cc78a3f4365031a5d52e251916162468d",
-        "webp": "76cd85b72a3ff2fdfb3d84f9153d3bccff7d7e0cbe0b6121865f099f4cd7aa11",
     }
     with TemporaryDirectory() as tmpdir:
         await create_report(
@@ -94,8 +90,6 @@ async def test_multiple_workflows(context: StreamFlowContext) -> None:
     digests = {
         "csv": "f604f9cf7af0841d667a6e549c7fd2125b2d30b19fd85abeef18ec3f27291ad8",
         "json": "90de1134abb06ed5c69d5ebf8221a2a69647acaa51d87cdb0448adc3ec1464be",
-        "png": "561c450e05d61c46b4018adca71ba3e2c28fc648d5cf833e797d5f1a60479fa3",
-        "webp": "831c84a5d25377ac65f5034c54a15f9f5784806d381cdfa06efe750f325379f7",
     }
     with TemporaryDirectory() as tmpdir:
         await create_report(
