a27b39f1e7d618864f6d471d2a189fc4392a410a
LanderOtto <alberto.mulone@unito.it>
2025-02-01 00:25:48 -0500

    Verified that new plots are valid and checksum updated. Moreover, a new tests is added for `group-by-name` option.

--- tests/test_report.py
+++ tests/test_report.py
@@ -36,7 +36,7 @@ async def test_single_workflow_single_execution(
     """Test producing a report for a single execution of a workflow."""
     digests = {
         "csv": "05ea34f00c0351d7812395abb175b6f3a98e131c4fcbc82053a940a853e08582",
-        "json": "992e2d10f1b0c5ed1bac5574cad607389f675522e993cf80719fc9995c2c2912",
+        "json": "d905527f526dd281a3731537f2fe6b0c0c5ed3cee04c28bbe09813b969cfb915",
     }
     with TemporaryDirectory() as tmpdir:
         await create_report(
@@ -58,11 +58,40 @@ async def test_single_workflow_single_execution(
 
 
 @pytest.mark.asyncio
+async def test_single_workflow_group_by_step(
+    context: StreamFlowContext,
+) -> None:
+    """Test producing a report for a single execution of a workflow grouped by step."""
+    digests = {
+        "csv": "05ea34f00c0351d7812395abb175b6f3a98e131c4fcbc82053a940a853e08582",
+        "json": "25bb543e9c6f22bf50f51a7bf65241dd79bb56b5355e7ec8b08694b71a8e1c54",
+    }
+    with TemporaryDirectory() as tmpdir:
+        await create_report(
+            context,
+            report_parser.parse_args(
+                [
+                    "wf_scatter_two_dotproduct",
+                    "--outdir",
+                    tmpdir,
+                    "--group-by-step",
+                    "--format",
+                ]
+                + list(digests.keys())
+            ),
+        )
+        for fmt in digests.keys():
+            report = os.path.join(tmpdir, f"report.{fmt}")
+            assert os.path.exists(report)
+            assert hashlib.sha256(open(report, "rb").read()).hexdigest() == digests[fmt]
+
+
+@pytest.mark.asyncio
 async def test_single_workflow_all_instances(context: StreamFlowContext) -> None:
     """Test producing a report for all executions of a workflow."""
     digests = {
         "csv": "aeb96671257cf1ec501e8c33b568d69c98948a7cd72e1b8c048015a7faf71054",
-        "json": "fc8261cf1df845a12e8738ff115773e0f3f73f38631c03942bf49830afb9c423",
+        "json": "f727d683557f364b27e15e0d85e6109a699648820710db760852f0f48662e368",
     }
     with TemporaryDirectory() as tmpdir:
         await create_report(
@@ -89,7 +118,7 @@ async def test_multiple_workflows(context: StreamFlowContext) -> None:
     """Test producing a report for multiple workflows."""
     digests = {
         "csv": "f604f9cf7af0841d667a6e549c7fd2125b2d30b19fd85abeef18ec3f27291ad8",
-        "json": "90de1134abb06ed5c69d5ebf8221a2a69647acaa51d87cdb0448adc3ec1464be",
+        "json": "7a78ee1f7440a6d4a4fff33ffd34a5bc38675307d11a75119ced836c9aa56da0",
     }
     with TemporaryDirectory() as tmpdir:
         await create_report(
