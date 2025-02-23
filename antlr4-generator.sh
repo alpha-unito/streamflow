fb884079bc39f89058f00a176d5dd4aa0dd51e8c
Alberto Mulone <48457093+LanderOtto@users.noreply.github.com>
2025-02-23 10:21:15 -0500

    Fix `ANTLR4` script generator (#674)
    This commit fixes the `ANTLR4` script generator by updating the
    URL pointing to the `ECMAScript.g4` grammar.

--- antlr4-generator.sh
+++ antlr4-generator.sh
@@ -6,7 +6,7 @@ WORKDIR=$(mktemp -d)
 
 cd "${WORKDIR}" || exit
 curl -fsSLO "https://www.antlr.org/download/antlr-${ANTLR4_VERSION}-complete.jar"
-curl -fsSLO "https://raw.githubusercontent.com/antlr/grammars-v4/master/javascript/ecmascript/Python/ECMAScript.g4"
+curl -fsSLO "https://raw.githubusercontent.com/antlr/grammars-v4/master/javascript/ecmascript/Python3/ECMAScript.g4"
 java -jar "antlr-${ANTLR4_VERSION}-complete.jar" -Dlanguage=Python3 ECMAScript.g4
 mv ./*.py "${SCRIPT_DIRECTORY}/streamflow/cwl/antlr/"
 rm -rf "${WORKDIR}"
