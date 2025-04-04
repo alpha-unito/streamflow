1f371b81a703ba8e2443157806e7362311babd0f
LanderOtto <alberto.mulone@unito.it>
2025-04-04 16:59:06 -0400

    Refactor

--- streamflow/config/config.py
+++ streamflow/config/config.py
@@ -10,32 +10,55 @@ from streamflow.core.config import Config
 from streamflow.core.exception import WorkflowDefinitionException
 from streamflow.core.workflow import Workflow
 from streamflow.log_handler import logger
+from streamflow.workflow.step import InputInjectorStep
 
 
 def _check_bindings(
     current_node: MutableMapping[str, Any], workflow: Workflow, path: str
 ) -> None:
     if logger.isEnabledFor(logging.WARNING):
-        if not any(step.startswith(path) for step in workflow.steps.keys()):
-            # Binding:
-            # - on the `step` bind check if there is a step named /parent/step_name
-            #   (it includes also sub-workflow bindings)
-            # - on the `port` bind check if there is a step called /port_name-injector
-            logger.warning(
-                f"Binding {path}, defined in the StreamFlow file, does not match any steps or ports in the workflow"
-            )
+        if "step" in current_node and not current_node.get("inherited_target", False):
+            if not any(step.startswith(path) for step in workflow.steps.keys()):
+                # The `step` bind check if there is a step named /parent/step_name
+                #   (it includes also sub-workflow bindings)
+                logger.warning(
+                    f"Binding {path}, defined in the StreamFlow file, does not match any steps in the workflow"
+                )
+        if "port" in current_node:
+            if not (
+                (
+                    # The port is an input port of the workflow
+                    posixpath.dirname(path) == posixpath.sep
+                    and any(
+                        posixpath.basename(path) in s.input_ports
+                        for s in workflow.steps.values()
+                        if isinstance(s, InputInjectorStep)
+                    )
+                )
+                or (
+                    # The port is an output port of a step
+                    posixpath.dirname(path) in workflow.steps
+                    and posixpath.basename(path)
+                    in workflow.steps[posixpath.dirname(path)].output_ports
+                )
+            ):
+                logger.warning(
+                    f"Binding {path}, defined in the StreamFlow file, does not match any ports in the workflow"
+                )
         for key, node in current_node["children"].items():
             _check_bindings(node, workflow, posixpath.join(path, key))
 
 
 def check_bindings(workflow_config: WorkflowConfig, workflow: Workflow) -> None:
-    _check_bindings(workflow_config.filesystem, workflow, posixpath.sep)
+    if len(node := workflow_config.filesystem["children"]) == 1:
+        _check_bindings(node[posixpath.sep], workflow, posixpath.sep)
 
 
 def set_targets(current_node, target):
     for node in current_node["children"].values():
         if "step" not in node:
             node["step"] = target
+            node["inherited_target"] = True
         set_targets(node, node["step"])
 
 
