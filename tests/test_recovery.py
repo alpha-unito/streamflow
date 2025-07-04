c9cbbc9e64d4fb4fd1392fb9169c22e8f8e97d6f
LanderOtto <alberto.mulone@unito.it>
2025-07-04 14:50:04 +0200

    This commit refactors the steps to call the `FailureManager`

--- tests/test_recovery.py
+++ tests/test_recovery.py
@@ -31,7 +31,7 @@ from tests.utils.workflow import (
     create_workflow,
 )
 
-TASK_FAILURE = ["execute", "transfer"]
+FAILURE_STEP = ["execute", "transfer"]
 NUM_STEPS = {"single_step": 1, "pipeline": 4}
 NUM_FAILURES = {"one_failure": 1, "two_failures_in_row": 2}
 ERROR_TYPE = [InjectorFailureCommand.SOFT_ERROR, InjectorFailureCommand.FAIL_STOP]
@@ -86,24 +86,24 @@ async def fault_tolerant_context(
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "num_of_steps,task,error_t,num_of_failures,token_t",
+    "num_of_steps,failure_step,error_type,num_of_failures,token_type",
     itertools.product(
-        NUM_STEPS.values(), TASK_FAILURE, ERROR_TYPE, NUM_FAILURES.values(), TOKEN_TYPE
+        NUM_STEPS.values(), FAILURE_STEP, ERROR_TYPE, NUM_FAILURES.values(), TOKEN_TYPE
     ),
     ids=[
         f"{n_step}_{step_t}_{error_t}_{n_failure}_{token_t}"
         for n_step, step_t, error_t, n_failure, token_t in itertools.product(
-            NUM_STEPS.keys(), TASK_FAILURE, ERROR_TYPE, NUM_FAILURES.keys(), TOKEN_TYPE
+            NUM_STEPS.keys(), FAILURE_STEP, ERROR_TYPE, NUM_FAILURES.keys(), TOKEN_TYPE
         )
     ],
 )
 async def test_execute(
     fault_tolerant_context: StreamFlowContext,
-    task: str,
-    num_of_steps: int,
-    error_t: str,
+    failure_step: str,
+    error_type: str,
     num_of_failures: int,
-    token_t: str,
+    num_of_steps: int,
+    token_type: str,
 ):
     deployment_t = "local"
     workflow = next(iter(await create_workflow(fault_tolerant_context, num_port=0)))
@@ -112,9 +112,9 @@ async def test_execute(
         "local": await get_deployment_config(fault_tolerant_context, deployment_t)
     }
     input_ports = {}
-    if token_t == "primitive":
+    if token_type == "primitive":
         token_value = 100
-    elif token_t == "file":
+    elif token_type == "file":
         location = await get_location(fault_tolerant_context, deployment_t)
         path = StreamFlowPath(
             tempfile.gettempdir() if location.local else "/tmp",
@@ -130,8 +130,10 @@ async def test_execute(
             "path": str(path),
             "size": await path.size(),
         }
+    elif token_type == "list":
+        token_value = list(range(5))
     else:
-        raise RuntimeError(f"Unknown token type: {token_t}")
+        raise RuntimeError(f"Unknown token type: {token_type}")
     for input_ in ("test",):
         injector_step = translator.get_base_injector_step(
             ["local"], input_, posixpath.join(posixpath.sep, input_), workflow
@@ -141,21 +143,19 @@ async def test_execute(
         injector_step.get_input_port(input_).put(TerminationToken())
     execute_steps = []
     for i in range(num_of_steps):
+        input_port = next(iter(input_ports.keys()))
         execute_steps.append(
             translator.get_execute_pipeline(
-                workflow,
-                os.path.join(posixpath.sep, str(i), utils.random_name()),
-                ["local"],
-                input_ports,
-                ["test1"],
-                transfer_failures=num_of_failures if task == "transfer" else 0,
-                failure_t=error_t,
+                command=f"lambda x : ('copy', '{token_type}', x['{input_port}'].value)",
+                deployment_names=["local"],
+                input_ports=input_ports,
+                outputs={"test1": token_type},
+                step_name=os.path.join(posixpath.sep, str(i), utils.random_name()),
+                workflow=workflow,
+                failure_tags={"0": num_of_failures},
+                failure_step=failure_step,
+                failure_type=error_type,
             )
-        )
-        execute_steps[-1].command = InjectorFailureCommand(
-            execute_steps[-1],
-            {"0": num_of_failures} if task == "execute" else {},
-            failure_t=error_t,
         )
         input_ports = execute_steps[-1].get_output_ports()
     await workflow.save(fault_tolerant_context)
@@ -189,13 +189,13 @@ async def test_execute(
                 DefaultFailureManager, fault_tolerant_context.failure_manager
             )._retry_requests[job_name]
             expected_failures = num_of_failures
-            if error_t == InjectorFailureCommand.FAIL_STOP:
-                if token_t != "primitive":
+            if error_type == InjectorFailureCommand.FAIL_STOP:
+                if token_type != "primitive":
                     if num_of_steps > 1 and num_of_failures > 1:
-                        # The failure of a step involves only the previous step
-                        expected_failures += (
-                            num_of_steps - int(step.name.split(os.sep)[1]) - 1
-                        )
+                        # Domino effect for each failure
+                        expected_failures = (
+                            num_of_steps - int(step.name.split(os.sep)[1])
+                        ) * 2
                     else:
                         # The failure of a step involves to all the previous steps
                         expected_failures *= num_of_steps - int(
@@ -237,13 +237,13 @@ async def test_scatter(fault_tolerant_context: StreamFlowContext):
     injector_step.get_input_port(input_).put(TerminationToken())
     # ExecuteStep
     step = translator.get_execute_pipeline(
-        workflow,
-        os.path.join(posixpath.sep, utils.random_name()),
-        ["local"],
-        {input_: injector_step.get_output_port(input_)},
-        [output_port],
+        command="lambda x : ('copy', 'list', x['test'].value)",
+        deployment_names=["local"],
+        input_ports={input_: injector_step.get_output_port(input_)},
+        outputs={output_port: "list"},
+        step_name=os.path.join(posixpath.sep, utils.random_name()),
+        workflow=workflow,
     )
-    step.command = InjectorFailureCommand(step)
     # ScatterStep
     scatter_step = workflow.create_step(
         cls=ScatterStep, name=utils.random_name() + "-scatter"
@@ -252,13 +252,13 @@ async def test_scatter(fault_tolerant_context: StreamFlowContext):
     scatter_step.add_output_port(output_port, workflow.create_port())
     # ExecuteStep
     step = translator.get_execute_pipeline(
-        workflow,
-        os.path.join(posixpath.sep, utils.random_name()),
-        ["local"],
-        {output_port: scatter_step.get_output_port(output_port)},
-        [output_port],
+        command=f"lambda x : ('copy', 'list', x['{output_port}'].value)",
+        deployment_names=["local"],
+        input_ports={output_port: scatter_step.get_output_port(output_port)},
+        outputs={output_port: "file"},
+        step_name=os.path.join(posixpath.sep, utils.random_name()),
+        workflow=workflow,
     )
-    step.command = InjectorFailureCommand(step)
     # GatherStep
     gather_step = workflow.create_step(
         cls=GatherStep,
@@ -269,14 +269,15 @@ async def test_scatter(fault_tolerant_context: StreamFlowContext):
     gather_step.add_output_port(output_port, workflow.create_port())
     # ExecuteStep
     step = translator.get_execute_pipeline(
-        workflow,
-        os.path.join(posixpath.sep, utils.random_name()),
-        ["local"],
-        gather_step.get_output_ports(),
-        [output_port],
-    )
-    step.command = InjectorFailureCommand(
-        step, {"0": num_of_failures}, failure_t=InjectorFailureCommand.FAIL_STOP
+        command=f"lambda x : ('copy', 'list', x['{output_port}'].value)",
+        deployment_names=["local"],
+        input_ports=gather_step.get_output_ports(),
+        outputs={output_port: "list"},
+        step_name=os.path.join(posixpath.sep, utils.random_name()),
+        workflow=workflow,
+        failure_step="execute",
+        failure_tags={"0": num_of_failures},
+        failure_type=InjectorFailureCommand.FAIL_STOP,
     )
     # Run
     await workflow.save(fault_tolerant_context)
@@ -353,18 +354,16 @@ async def test_synchro(fault_tolerant_context: StreamFlowContext):
     for _ in range(num_of_steps):
         execute_steps.append(
             translator.get_execute_pipeline(
-                workflow,
-                os.path.join(posixpath.sep, utils.random_name()),
-                ["local"],
-                input_ports,
-                ["test1"],
-                transfer_failures=num_of_failures if step_t == "transfer" else 0,
+                command="lambda x : ('copy', 'file', x['test'].value)",
+                deployment_names=["local"],
+                input_ports=input_ports,
+                outputs={"test1": token_t},
+                step_name=os.path.join(posixpath.sep, utils.random_name()),
+                workflow=workflow,
+                failure_step=step_t,
+                failure_tags={"0": num_of_failures},
+                failure_type=InjectorFailureCommand.INJECT_TOKEN,
             )
-        )
-        execute_steps[-1].command = InjectorFailureCommand(
-            execute_steps[-1],
-            {"0": num_of_failures} if step_t == "execute" else {},
-            failure_t=InjectorFailureCommand.INJECT_TOKEN,
         )
         input_ports = execute_steps[-1].get_output_ports()
     await workflow.save(fault_tolerant_context)
