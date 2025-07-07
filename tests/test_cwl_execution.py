f3198e0fd31f38c3c5b3b50b39c8cbbacaa6ac6e
LanderOtto <alberto.mulone@unito.it>
2025-07-07 09:48:48 +0200

    Updated test

--- tests/test_cwl_execution.py
+++ tests/test_cwl_execution.py
@@ -7,10 +7,9 @@ import pytest
 
 from streamflow.core import utils
 from streamflow.core.context import StreamFlowContext
-from streamflow.core.data import DataType
 from streamflow.cwl.command import CWLCommand, CWLCommandTokenProcessor
 from streamflow.cwl.hardware import CWLHardwareRequirement
-from streamflow.cwl.step import CWLExecuteStep
+from streamflow.cwl.step import CWLExecuteStep, CWLInputInjectorStep
 from streamflow.cwl.token import CWLFileToken
 from streamflow.cwl.translator import create_command_output_processor_base
 from streamflow.cwl.workflow import CWLWorkflow
@@ -19,7 +18,7 @@ from streamflow.workflow.executor import StreamFlowExecutor
 from streamflow.workflow.token import ListToken, ObjectToken
 from streamflow.workflow.utils import get_job_token
 from tests.utils.cwl import get_cwl_parser
-from tests.utils.deployment import get_location
+from tests.utils.deployment import get_local_deployment_config, get_location
 from tests.utils.utils import inject_tokens
 from tests.utils.workflow import (
     CWL_VERSION,
@@ -32,7 +31,7 @@ from tests.utils.workflow import (
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "token_type,file_type",
-    itertools.product(("file", "list", "object"), ("file", "dir")),
+    itertools.product(("file", "list", "object"), ("file", "dir", "listing")),
 )
 async def test_initial_workdir(
     context: StreamFlowContext, token_type: str, file_type: str
@@ -42,33 +41,62 @@ async def test_initial_workdir(
     Copy the input files to the job's working directory and compute the echo of the paths.
     Verify that the files exist in the directory and that the printed paths match the expected values.
     """
-    workflow, (in_port_schedule, in_port, out_port) = await create_workflow(
-        context, num_port=3
-    )
+    in_port_name = "in_1"
+    out_port_name = "out-1"
     step_name = posixpath.join(posixpath.sep, utils.random_name())
-    deploy_step = create_deploy_step(workflow)
+    # Get deployment
+    deployment_config = get_local_deployment_config()
+    execution_location = await get_location(
+        context, deployment_t=deployment_config.type
+    )
+    # Create workflow
+    workflow, (in_port, out_port) = await create_workflow(context, num_port=2)
+    deploy_step = create_deploy_step(
+        workflow=workflow, deployment_config=deployment_config
+    )
+    injector_schedule_step = create_schedule_step(
+        workflow,
+        [deploy_step],
+        name_prefix=step_name + "-injector/__schedule__",
+    )
+    injector = workflow.create_step(
+        cls=CWLInputInjectorStep,
+        name=step_name + "-injector",
+        job_port=injector_schedule_step.get_output_port(),
+    )
+    injector.add_input_port(in_port_name, in_port)
+    injector.add_output_port(in_port_name, workflow.create_port())
     schedule_step = create_schedule_step(
         workflow,
         [deploy_step],
         name_prefix=step_name,
         hardware_requirement=CWLHardwareRequirement(cwl_version=CWL_VERSION),
     )
-    in_port_name = "in_1"
-    out_port_name = "out-1"
+    schedule_step.add_input_port(in_port_name, injector.get_output_port())
     execute_step = workflow.create_step(
         cls=CWLExecuteStep,
         name=step_name,
         job_port=schedule_step.get_output_port(),
         recoverable=True,
     )
-    arg = f"$(inputs.{in_port_name}" + (".a)" if token_type == "object" else ")")
+    workdir_expression = f"$(inputs.{in_port_name}"
+    if token_type == "object":
+        workdir_expression += ".a"
+    if file_type == "listing":
+        if token_type == "list":
+            workdir_expression += "[0]"
+        workdir_expression += ".listing"
+    workdir_expression += ")"
     execute_step.command = CWLCommand(
         step=execute_step,
         base_command=["echo"],
-        processors=[CWLCommandTokenProcessor(name=in_port_name, expression=arg)],
+        processors=[
+            CWLCommandTokenProcessor(name=in_port_name, expression=workdir_expression)
+        ],
         full_js=True,
-        initial_work_dir=arg,
+        initial_work_dir=workdir_expression,
     )
+    execute_step.add_input_port(in_port_name, injector.get_output_port())
     execute_step.add_output_port(
         name=out_port_name,
         port=out_port,
@@ -83,7 +111,7 @@ async def test_initial_workdir(
             context={"hints": {}, "requirements": {}, "version": CWL_VERSION},
         ),
     )
-    execution_location = await get_location(context, "local")
+    # Create input data
     path = None
     try:
         if file_type == "file":
@@ -92,27 +120,49 @@ async def test_initial_workdir(
             )
             await path.write_text("Hello world")
             path = await path.resolve()
-        else:
+            file_token = CWLFileToken(
+                {
+                    "class": "File",
+                    "path": str(path),
+                }
+            )
+            initial_paths = [str(path)]
+        elif file_type in ("dir", "listing"):
             path = StreamFlowPath(
                 "/tmp", "land", context=context, location=execution_location
             )
             await path.mkdir()
             path = await path.resolve()
             await (path / "lvl1").mkdir()
-            await (path / "lvl2").mkdir(exist_ok=True)
+            await (path / "lvl2").mkdir()
             await (path / "lvl1" / "text.txt").write_text("Hello dir")
-        file_token = CWLFileToken(
-            {
-                "class": "File" if file_type == "file" else "Directory",
-                "path": str(path),
-            }
-        )
-        context.data_manager.register_path(
-            location=execution_location,
-            path=str(path),
-            relpath=str(path),
-            data_type=DataType.PRIMARY,
-        )
+            file_token = CWLFileToken(
+                {
+                    "class": "Directory",
+                    "path": str(path),
+                    "listing": [
+                        {
+                            "class": "Directory",
+                            "path": str(path / "lvl1"),
+                            "listing": [
+                                {
+                                    "class": "File",
+                                    "path": str(path / "lvl1" / "text.txt"),
+                                }
+                            ],
+                        },
+                        {"class": "Directory", "path": str(path / "lvl2")},
+                    ],
+                }
+            )
+            initial_paths = (
+                [str(path)]
+                if file_type == "dir"
+                else [str(path / "lvl1"), str(path / "lvl2")]
+            )
+        else:
+            raise NotImplementedError(f"Unsupported file type: {file_type}")
+        # Inject input token
         if token_type == "file":
             token_value = file_token
         elif token_type == "list":
@@ -121,12 +171,8 @@ async def test_initial_workdir(
             token_value = ObjectToken({"a": file_token})
         else:
             raise ValueError(f"Invalid token_type: {token_type}")
-        execute_step.add_input_port(in_port_name, in_port)
         await inject_tokens([token_value], in_port, context)
-
-        schedule_step.add_input_port(in_port_name, in_port_schedule)
-        await inject_tokens([token_value], in_port_schedule, context)
-
+        # Execute workflow
         await workflow.save(context)
         executor = StreamFlowExecutor(workflow)
         await executor.run()
@@ -137,15 +183,19 @@ async def test_initial_workdir(
             posixpath.join(execute_step.name, "0"),
             schedule_step.get_output_port().token_list,
         ).value
-        # Check whether the file has been copied to the job output directory
         outdir = StreamFlowPath(
             job.output_directory, context=context, location=execution_location
         )
-        async for f in outdir.glob("*"):
-            assert f == token.value
-        #  Check if the command argument has the correct path
-        assert token.value != str(path)
-        assert token.value == os.path.join(job.output_directory, path.parts[-1])
+        files_found = [str(f) async for f in outdir.glob("*")]
+        for out_file, init_path in zip(token.value.split(" "), initial_paths):
+            # Check whether the file has been copied to the job output directory
+            files_found.remove(out_file)
+            #  Check whether the command argument has the correct path
+            assert out_file != str(init_path)
+            assert out_file == os.path.join(
+                job.output_directory, os.path.basename(init_path)
+            )
+        assert len(files_found) == 0
     finally:
         if path:
             await path.rmtree()
