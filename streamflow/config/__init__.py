72b3c743710f05d2edfb4ef3ad8f19585b525071
GlassOfWhiskey <iacopo.c92@gmail.com>
2025-10-05 09:29:20 +0200

    Add support for Python 3.14
    This commit introduces official support for Python 3.14 in the
    StreamFlow CI/CD tests, manifest, Docker containers, and docs.

--- streamflow/config/__init__.py
+++ streamflow/config/__init__.py
@@ -1,6 +1,12 @@
+import sys
 from collections.abc import MutableSequence
-from importlib.abc import Traversable
 from importlib.resources import files
+
+if sys.version_info < (3, 11):
+    from importlib.abc import Traversable
+else:
+    from importlib.resources.abc import Traversable
+
 
 ext_schemas: MutableSequence[Traversable] = [
     files("streamflow.deployment.connector")
