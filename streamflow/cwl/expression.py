0fb1d62007c3501c219216311bac3d25f53f1918
LanderOtto <alberto.mulone@unito.it>
2025-11-17 17:41:24 +0100

    This commit fixes an issue where the `deps` attribute value was incorrectly overwritten.

--- streamflow/cwl/expression.py
+++ streamflow/cwl/expression.py
@@ -100,7 +100,7 @@ class DependencyResolver:
         listener = CWLDependencyListener()
         walker = antlr4.ParseTreeWalker()
         walker.walk(listener, parser.program())
-        self.deps = listener.deps
+        self.deps |= listener.deps
 
     def regex_eval(
         self,
