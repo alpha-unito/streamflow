alpha-unito/streamflow

commit 04b76580d1fdb70a4f66157e119f9fff081763bd

Fix optional empty list output in CWL (#959)
This commit fixes `CWLCommandOutputProcessor` to handle empty optional
lists. Before this commit, the processor returned an empty list even when the
output was defined as optional. This commit updates the logic to return a null
token instead of an empty list in these cases, aligning with `cwltool` behavior.

streamflow/cwl/processor.py
@@ -534,8 +534,9 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
                     globpath if isinstance(globpath, MutableSequence) else [globpath]
                 )
             # Resolve glob
+            location_globpaths: MutableMapping[str, list[MutableMapping[str, Any]]] = {}
             for location in locations:
-                globpaths = dict(
+                expanded_globs = dict(
                     flatten_list(
                         await asyncio.gather(
                             *(
@@ -568,10 +569,10 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
                     )
                 )
                 # Get token class from paths
-                globpaths = [
+                location_globpaths[location.name] = [
                     {"path": p, "class": c}
                     for p, c in zip(
-                        globpaths.keys(),
+                        expanded_globs.keys(),
                         await asyncio.gather(
                             *(
                                 asyncio.create_task(
@@ -579,7 +580,7 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
                                         p, job, self.workflow.context
                                     )
                                 )
-                                for p in globpaths.values()
+                                for p in expanded_globs.values()
                             )
                         ),
                         strict=True,
@@ -596,7 +597,7 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
                     secondary_files=self.secondary_files,
                     connector=connector,
                     locations=locations,
-                    token_value=globpaths,
+                    token_value=flatten_list(location_globpaths.values()),
                     load_contents=self.load_contents,
                     load_listing=self.load_listing,
                 )
@@ -616,7 +617,7 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
                     secondary_files=self.secondary_files,
                     connector=connector,
                     locations=locations,
-                    token_value=globpaths,
+                    token_value=flatten_list(location_globpaths.values()),
                     load_contents=self.load_contents,
                     load_listing=self.load_listing,
                 )
@@ -793,7 +794,11 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
         token = await self._build_token(
             job, connector, context, token_value, recoverable
         )
-        if self.single or isinstance(token, ListToken):
+        if (
+            self.single
+            or isinstance(token, ListToken)
+            or (self.optional and token.value is None)
+        ):
             return token
         else:
             return ListToken(

.github/workflows/ci-tests.yaml
@@ -40,24 +40,24 @@ jobs:
           - commit: "76bdf9b55e2378432e0e6380ccedebb4a94ce483"
             exclude: "docker_entrypoint,modify_file_content"
             version: "v1.2"
-          - commit: "2063e9095f421f2a2ce12abaf196b2ba06ca5aae"
+          - commit: "e61e6e2c37032ac6675b3ef8664f60c93bbc810a"
             exclude: "docker_entrypoint,modify_file_content"
             version: "v1.3"
           - docker: "singularity"
-            commit: "2063e9095f421f2a2ce12abaf196b2ba06ca5aae"
+            commit: "e61e6e2c37032ac6675b3ef8664f60c93bbc810a"
             exclude: "docker_entrypoint,modify_file_content,iwd-container-entryname1"
             on: "ubuntu-24.04"
             python: "3.14"
             version: "v1.3"
           - docker: "kubernetes"
-            commit: "2063e9095f421f2a2ce12abaf196b2ba06ca5aae"
+            commit: "e61e6e2c37032ac6675b3ef8664f60c93bbc810a"
             exclude: "docker_entrypoint,modify_file_content"
             on: "ubuntu-24.04"
             python: "3.14"
             version: "v1.3"
           - on: "macos-15-intel"
             python: "3.14"
-            commit: "2063e9095f421f2a2ce12abaf196b2ba06ca5aae"
+            commit: "e61e6e2c37032ac6675b3ef8664f60c93bbc810a"
             exclude: "docker_entrypoint,modify_file_content"
             version: "v1.3"
     runs-on: ${{ matrix.on }}

commit 827069c79b01428fb960aaec976d03ba47ed0b88

This commit fixes an issue introduced in #959. Before this commit, when an output was mandatory but the glob found no files, an empty token was propagated. Now, if no files match: a list is propagated for mandatory outputs, while an empty token is propagated for optional outputs.
Moreover, this commit streamlines command output processing by removing the manual manipulation of glob results and instead leveraging the single logic already present.

This same issue with empty globs was previously solved in #472; a regression test has now been added to prevent future occurrences.

streamflow/cwl/processor.py
@@ -600,11 +600,7 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
                     load_contents=self.load_contents,
                     load_listing=self.load_listing,
                 )
-                return (
-                    token_list
-                    if len(token_list) > 1
-                    else token_list[0] if len(token_list) == 1 else None
-                )
+                return token_list
             # Otherwise, fill context['self'] with glob data and proceed
             else:
                 context["self"] = await utils.build_token_value(
@@ -756,7 +752,9 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
             )
         if self.token_type != "Any":  # nosec
             if isinstance(token_value, MutableSequence):
-                if self.single:
+                if self.optional and len(token_value) == 0:
+                    token_value = None
+                elif self.single:
                     if len(token_value) == 1:
                         token_value = token_value[0]
                         _check_token_type(
@@ -800,10 +798,7 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
         ):
             return token
         else:
-            return ListToken(
-                value=[token] if token.value is not None else [],
-                tag=token.tag,
-            )
+            return ListToken(value=token, tag=token.tag)
 
 
 class CWLObjectCommandOutputProcessor(ObjectCommandOutputProcessor):

streamflow/cwl/translator.py
@@ -257,7 +257,7 @@ def _create_command_output_processor(
             cwl_name_prefix=cwl_name_prefix,
             schema_def_types=schema_def_types,
             context=context,
-            optional=True,
+            optional=optional,
             single=False,
         )
     # Enum type: -> create command output processor