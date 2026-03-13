2064828c02b54d984a10966a90520272faa431b0
Alberto Mulone <48457093+LanderOtto@users.noreply.github.com>
2026-03-13 18:28:48 +0100

    Fix empty glob (#989)
    This commit fixes an issue introduced in #959. Before this commit,
    when an output was a mandatory list of files but the glob found no
    files, an empty token was propagated. Now, if no files match: a list
    is propagated for mandatory outputs, while an empty token is
    propagated for optional outputs.
    
    Moreover, this commit streamlines command output processing by
    removing the manual manipulation of glob results and instead
    leveraging the `single` logic already present in the
    `CWLCommandOutputProcessor`.
    
    This same issue with empty globs was previously solved in #472; a
    regression test has now been added to prevent future occurrences.

--- streamflow/cwl/processor.py
+++ streamflow/cwl/processor.py
@@ -600,11 +600,7 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
                     load_contents=self.load_contents,
                     load_listing=self.load_listing,
                 )
-                return (
-                    token_list
-                    if len(token_list) > 1
-                    else token_list[0] if len(token_list) == 1 else None
-                )
+                return token_list
             # Otherwise, fill context['self'] with glob data and proceed
             else:
                 context["self"] = await utils.build_token_value(
@@ -756,7 +752,9 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
             )
         if self.token_type != "Any":  # nosec
             if isinstance(token_value, MutableSequence):
-                if self.single:
+                if self.optional and len(token_value) == 0:
+                    token_value = None
+                elif self.single:
                     if len(token_value) == 1:
                         token_value = token_value[0]
                         _check_token_type(
@@ -790,20 +788,9 @@ class CWLCommandOutputProcessor(CommandOutputProcessor):
                     optional=self.optional,
                     check_file=True,
                 )
-        token = await self._build_token(
+        return await self._build_token(
             job, connector, context, token_value, recoverable
         )
-        if (
-            self.single
-            or isinstance(token, ListToken)
-            or (self.optional and token.value is None)
-        ):
-            return token
-        else:
-            return ListToken(
-                value=[token] if token.value is not None else [],
-                tag=token.tag,
-            )
 
 
 class CWLObjectCommandOutputProcessor(ObjectCommandOutputProcessor):
