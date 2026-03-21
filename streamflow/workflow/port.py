4ec8aaf0e124f759540182f9b9c56a52ddb0166e
LanderOtto <alberto.mulone@unito.it>
2026-03-21 17:26:33 +0100

    This commit improves the `InterWorkflowPort` supporting a list of expected tags, instead of a single upper tag. Before this commit, the scatter can be prone to errors when the max tag is generated before the other ones.

--- streamflow/workflow/port.py
+++ streamflow/workflow/port.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Callable, MutableMapping, MutableSequence
+from collections.abc import Callable, MutableSequence
 from enum import Flag, auto
 from typing import NamedTuple
 
@@ -11,9 +11,15 @@ from streamflow.log_handler import logger
 from streamflow.workflow.token import TerminationToken
 
 
+class BoundaryAction(Flag):
+    PROPAGATE = auto()
+    TERMINATE = auto()
+
+
 class BoundaryRule(NamedTuple):
     port: Port
-    termination_type: TerminationType
+    action: BoundaryAction
+    tags: MutableSequence[str]
 
 
 class ConnectorPort(Port):
@@ -56,51 +62,57 @@ class FilterTokenPort(Port):
             logger.debug(f"Port {self.name} skips {token.tag}")
 
 
-class TerminationType(Flag):
-    PROPAGATE = auto()
-    TERMINATE = auto()
-
-
 class InterWorkflowPort(Port):
     def __init__(self, workflow: Workflow, name: str):
         super().__init__(workflow, name)
-        self.boundaries: MutableMapping[str, MutableSequence[BoundaryRule]] = {}
+        self.boundaries: MutableSequence[BoundaryRule] = []
 
-    def _handle_boundary(self, boundary: BoundaryRule, token: Token) -> None:
-        if TerminationType.PROPAGATE in boundary.termination_type:
-            if boundary.port is self:
-                super().put(token)
+    def _handle_boundary(self, boundary: BoundaryRule, token: Token) -> bool:
+        if token.tag in boundary.tags:
+            boundary.tags.remove(token.tag)
+            if len(boundary.tags) == 0:
+                if BoundaryAction.PROPAGATE in boundary.action:
+                    if boundary.port is self:
+                        super().put(token)
+                    else:
+                        boundary.port.put(token)
+                if BoundaryAction.TERMINATE in boundary.action:
+                    if boundary.port is self:
+                        super().put(TerminationToken(Status.RECOVERED))
+                    else:
+                        boundary.port.put(TerminationToken(Status.RECOVERED))
+                return True
             else:
-                boundary.port.put(token)
-        if TerminationType.TERMINATE in boundary.termination_type:
-            if boundary.port is self:
-                super().put(TerminationToken(Status.RECOVERED))
-            else:
-                boundary.port.put(TerminationToken(Status.RECOVERED))
+                return False
+        else:
+            # Duplicated tokens. e.g. size port of a gather step.
+            # first token is injected by the recovery system,
+            # second token is injected by the scatter size output
+            return False
 
     def add_inter_port(
         self,
         port: Port,
-        boundary_tag: str,
-        termination_type: TerminationType,
+        boundary_tags: MutableSequence[str],
+        boundary_action: BoundaryAction,
     ) -> None:
-        boundary = BoundaryRule(port, termination_type)
-        self.boundaries.setdefault(boundary_tag, []).append(boundary)
+        boundary = BoundaryRule(port=port, action=boundary_action, tags=boundary_tags)
+        self.boundaries.append(boundary)
 
         # Create a copy of `token_list` because the list can be modified within `_handle_self_boundary` method
         for token in list(self.token_list):
-            if token.tag == boundary_tag:
-                self._handle_boundary(boundary, token)
+            self._handle_boundary(boundary, token)
 
     def put(self, token: Token) -> None:
-        if (
-            isinstance(token, TerminationToken)
-            or token.tag not in self.boundaries.keys()
-        ):
+        if isinstance(token, TerminationToken):
             super().put(token)
         else:
-            for boundary in self.boundaries[token.tag]:
-                self._handle_boundary(boundary, token)
+            self_rule = False
+            for boundary in self.boundaries:
+                if self._handle_boundary(boundary, token) and boundary.port is self:
+                    self_rule = True
+            if not self_rule:
+                super().put(token)
 
 
 class InterWorkflowJobPort(InterWorkflowPort, JobPort):
