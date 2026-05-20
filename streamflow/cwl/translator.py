25a11c108fc3dd4680231811f120e03c7e3e557b
LanderOtto <alberto.mulone@unito.it>
2026-05-20 10:51:31 +0200

    Refactor

--- streamflow/cwl/translator.py
+++ streamflow/cwl/translator.py
@@ -1915,16 +1915,7 @@ class CWLTranslator:
         for input_port in workflow.ports.values():
             if input_port.empty() and not input_port.get_input_steps():
                 empty_ports.append(input_port)
-        if len(empty_ports) == 1:
-            self._inject_input(
-                workflow=workflow,
-                global_name="/__empty_unbound_inputs__",
-                port_name="__empty_unbound_inputs__",
-                port=empty_ports[0],
-                output_directory=output_directory,
-                value=None,
-            )
-        elif len(empty_ports) > 1:
+        if len(empty_ports) > 1:
             step = workflow.create_step(
                 cls=BroadcastTransformer,
                 name="/__empty_unbound_inputs__-bcast",
@@ -1933,14 +1924,16 @@ class CWLTranslator:
             step.add_input_port("__upstream__", upstream_port)
             for i, downstream_port in enumerate(empty_ports):
                 step.add_output_port(f"__downstream_{i}__", downstream_port)
-            self._inject_input(
-                workflow=workflow,
-                global_name="/__empty_unbound_inputs__",
-                port_name="__empty_unbound_inputs__",
-                port=upstream_port,
-                output_directory=output_directory,
-                value=None,
-            )
+        else:
+            upstream_port = empty_ports[0]
+        self._inject_input(
+            workflow=workflow,
+            global_name="/__empty_unbound_inputs__",
+            port_name="__empty_unbound_inputs__",
+            port=upstream_port,
+            output_directory=output_directory,
+            value=None,
+        )
 
     def _recursive_translate(
         self,
